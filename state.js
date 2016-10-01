5f496ac940343bfba35051dd2cebab1fbb290f69
Christopher Paul <cp11@umbc.edu>
2016-10-01 17:33:50 -0400

    added logic to update vote count and individual vote history

--- state.js
+++ state.js
@@ -6,6 +6,7 @@ function State(io) {
     this.choicesCount = 0;
     this.votes = {};
     this.counter = 0;
+    this.studentSubmit = {};
 }
 
 module.exports = State;
@@ -19,6 +20,7 @@ State.prototype.openPolls = function(choicesCount) {
     debug('opening votes');
     this.choicesCount = choicesCount;
     this.votes = {};
+    this.studentSubmit = {};
     this.pollingOpen = true;
     io.emit('pollingOpen', {choicesCount: choicesCount});
 }
@@ -31,6 +33,47 @@ State.prototype.closePolls = function() {
 
 State.prototype.recordVote = function(vote, uniqueid) {
     //TODO: do something
+
+    //if this user has never entered a vote before let's log it
+    if(!(uniqueid in studentSubmit)){
+        studentSubmit[uniqueid] = vote;
+        
+        //if this vote has never been chosen before
+        if(!(vote in this.votes)){
+            this.votes[vote] = 1;
+        }
+        
+        //if this vote has bee chose before
+        else{
+            this.votes[vote]++;
+        }
+    }
+    //if they have logged a vote before we need to update their new choice
+    else{
+
+        //get their previous submission which we will subtract the total count from later
+        previousChoice = studentSubmit[uniqueid];
+    
+        //If the choice they made is not the same as the choice they have made before
+        // basically if they have made a new choice then do something
+        if(previousChoice != vote){        
+
+            //update their key with their new vote choice
+            studentSubmit[uniqueid] = vote;
+
+            //if the overall count for a choice is 1 that means when we decrement it'll be 0
+            // so we want to delete that entry in the dictionary
+            if(this.votes[previousChoice] - 1 == 0){
+                delete this.votes[previousChoice];
+            }
+            
+            //if the count for this vote is more than one just decrement it's overall value
+            else{
+                this.votes[vote]--;
+            } 
+        }
+    }    
+        
     socket.emit('voteUpdate', {
         count: 492,
     });
