a4793d05003627032cc0ab044a4ed7e2b19d93db
Zack Orndorff <zack@zackorndorff.com>
2016-10-01 14:33:28 -0400

    Add client-side messaging test to presenter

--- socket.js
+++ socket.js
@@ -1,13 +1,18 @@
 var debug = require('debug')('Clack:socketio');
 
+function myHandler(socket) {
+    debug("user connected");
+    setTimeout(function() {
+        socket.emit('message', "hi");
+    }, 5000);
+    socket.on('message', function(msg) {
+        debug(msg);
+    });
+}
+
 module.exports = function(server) {
     // Socket.io
     var io = require('socket.io')(server);
 
-    io.on('connection', function(socket) {
-        debug("user connected");
-        setTimeout(function() {
-            socket.emit('message', "hi");
-        }, 5000);
-    });
+    io.on('connection', myHandler);
 }
